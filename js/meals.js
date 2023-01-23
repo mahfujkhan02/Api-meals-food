8dbe27392aacba768fc99734424876bf5c3d115e
mahfujkhan02 <mahfujtanim450@gmail.com>
2023-01-24 01:43:58 +0600

    dynamic url and display container  added

--- js/meals.js
+++ js/meals.js
@@ -52,10 +52,40 @@ const loadMealDetail = (idmeal) => {
   fetch(url)
 
   .then(res => res.json())
-  //.then(data => displayMealDetail(data.meals[0]))
-  .then(data => console.log(data.meals[0]))
+  then(data => displayMealDetail(data.meals[0]))
+  //.then(data => console.log(data.meals[0]))
 
 }
+
+const displayMealDetail = meal =>{
+
+  // for(const meal of mealss)
+  //console.log(meal)
+
+  const detailContainer = document.getElementById('detail-container')
+  detailContainer.innerHTML =``;
+
+  const mealDiv = document.createElement('div')
+  mealDiv.classList.add('card');
+
+  mealDiv.innerHTML = `
+
+  <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">${meal.strMeal}</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+
+  `;
+
+  detailContainer.appendChild(mealDiv);
+
+
+
+}
+       
+     
 
    loadMeals('');
 
